a1b2b1fd29eca91f220df641b8c99638092e6c44
Eric Reiche <e@reiche.me>
2013-03-03 20:42:00 +0100

    Added image embeding for image selector in admins.

--- Resources/public/js/filechooser.js
+++ Resources/public/js/filechooser.js
@@ -1,9 +1,37 @@
+
+function reloadImageChooser()
+{
+    var fileChooser = $('#filechooser');
+    fileChooser.load(fileChooserUrl, function() {
+        // loading done
+    });
+}
+
+function addImage(id)
+{
+    var link = "&#123;&#123; img(" + id + ", 150, 150) &#125;&#125;"
+    $('.btnTarget').append(link);
+}
+
+function addImageLink(id)
+{
+    var link = "&#123;&#123; imgLink(" + id + ", 150, 150) &#125;&#125;"
+    $('.btnTarget').append(link);
+}
+
 $(document).ready(function() {
-    function reloadImageChooser()
-    {
-        var fileChooser = $('#filechooser');
-        fileChooser.load(fileChooserUrl, function() {
-            // loading done
+
+    $('textarea').each(function(){
+        if($(this).attr('id').search(/content/i)) {
+            $(this).addClass('btnTarget');
+        }
+    });
+
+    var myDropzone = $(".dropzone").data("dropzone");
+
+    if (myDropzone) {
+        myDropzone.on("success", function(file) {
+            reloadImageChooser();
         });
     }
 });
